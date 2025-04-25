8ccfeadbd65f110065612fade0dc273537a4450d
Léo Vernin <leo.vernin@gmail.com>
2025-04-25 16:37:41 +0200

    Update helper.py
    added get_start_date

--- helper.py
+++ helper.py
@@ -17,3 +17,24 @@ def dtms(dt:datetime) -> int:
     """
     dt_utc = dt.astimezone(timezone.utc)
     return int(dt_utc.timestamp() * 1000)
+    
+def get_start_date():
+    print("Please enter the start date :")
+    year = int(input("Year (YYYY): "))
+    month = int(input("Month (1-12): "))
+    day = int(input("Day (1-31): "))
+    hour = int(input("Hour (0-23): "))
+    minute = int(input("Minute (0-59): "))
+    delta = int(input("Timezone difference from UTC: "))
+    
+    start_date = datetime(
+        year,          
+        month,       
+        day,          
+        hour,      
+        minute,         
+        second=0,     
+        microsecond=0,  
+        tzinfo=timezone(timedelta(hours=delta))   
+    )
+    return start_date
